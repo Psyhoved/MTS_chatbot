e1e6b0ceb4a8a1be03a6a381d9751d804e0883da
user <nilov@neo-s.com>
2023-08-14 04:25:41 +0300

    бэкенд настроен для работы с нейронкой

---
+++ backend/src/main/kotlin/com/vane/neurochat/configs/RestTemplateconfig.kt
@@ -0,0 +1,11 @@
+package com.vane.neurochat.configs
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateConfig {
+    @Bean
+    fun restTemplate() = RestTemplate()
+}
