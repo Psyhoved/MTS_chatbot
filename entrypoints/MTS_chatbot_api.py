b830e4fc2c7bd81e3a6261573ccdea1436379d6d
Aleksandr Lebedev <mr.alexandrlebedev@mail.ru>
2023-08-15 19:54:57 +0300

    рефакторинг MTS_chatbot_api.py

--- entrypoints/MTS_chatbot_api.py
+++ entrypoints/MTS_chatbot_api.py
@@ -1,25 +1,45 @@
-import os
+import logging
+import re
+import urllib.parse
+
+# Настройка логгера
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s - %(message)s')
+
+
+def check_russian(text: str) -> bool:
+    return bool(re.search('[а-яА-Я]', text))
+
 
 class ChatBot:
     def __init__(self):
-        self.instruction = """Ты - чат-бот на сайте, твоя функция помогать. Отвечай развернуто, вежливо. Если уместно,
-    то в конце своих ответов можешь задать уточняющий вопрос.
-    Если последний вопрос в запросе абсолютно не касается предоставленного контекста, то откажись от ответа на него!
-    Отвечай на вопросы про сервис Мобильные сотрудники, МТС Координатор и МТС Трекер."""
+        self.instruction = """
+        Ты - чат-бот Светлана и работаешь на сайте МТС "Мобильные сотрудники",
+        твоя функция - стараться ответить на любой вопрос клиента. 
+        Отвечай весело, позитивно, можешь шутить. 
+        Используй в ответах только русский язык! Не отвечай на английском! 
+        Если вопрос не касается контекста, то вежливо и дружелюбно переведи тему, 
+        и расскажи про сервис Мобильные сотрудники, МТС Координатор и МТС Трекер! 
+        Отвечай на вопросы про сервис Мобильные сотрудники, МТС Координатор и МТС Трекер.\n  
+    """
 
     def ask_bot(self, context, promt: str):
+        dop = ' Ответь на последний вопрос или позитивно поддержи диалог'
         query_engine = context.as_query_engine()
+        promt = urllib.parse.unquote(promt)
+        logging.info(f"То, что приходит: {promt}")
         try:
-            response = query_engine.query(self.instruction + promt)
+            response = query_engine.query(self.instruction + promt + dop)
         except Exception as e:
             response = f'К сожалению, не могу ответить на Ваш вопрос. Сервис недоступен: {e}'
             return response
 
         if not response.response or response.response == 'Empty Response':
             return "Не понял Вашего вопроса. Напишите, пожалуйста, точнее, чтобы Вы хотели узнать."
-        else:
-            self.instruction = promt + ' ' + response.response + ' '
-            return response.response
+        elif not check_russian(response.response):
+            response = query_engine.query(response.response + ' Переведи на русский')
+
+        return response.response
 
 
 
